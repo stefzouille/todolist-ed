4075149664a46c40d1cfc033ca44fbabe72d918b
stefzouille <contact.lokcy@gmail.com>
2021-04-26 21:53:21 +0200

    add

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
        <h1>Coconut's TodoList</h1>
      </header>
      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-     <TodoList todos={todos}/>
+     <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
