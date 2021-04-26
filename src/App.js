1186181196ac07e757ba62313cfd6cc32d387b8c
stefzouille <contact.lokcy@gmail.com>
2021-04-26 22:56:14 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,20 +1,71 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 //importing Components
 import Form from './components/Form'
 import TodoList from './components/TodoList';
 
 function App() {
+  
+  //state stuff
   const [inputText, setInputText] = useState("")
   const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setfilteredTodos] = useState([])
+
+  //run une fois quand l app start
+  useEffect(() => {
+    getLocalTodos()
+  }, [])
+//useEffect
+  useEffect(() => {
+    filterHandler()
+  }, [todos, status])
+
+  //function
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setfilteredTodos(todos.filter(todo => todo.completed === true))
+        break
+      case 'uncompleted':
+        setfilteredTodos(todos.filter(todo => todo.completed === false))
+        break
+        default:
+         setfilteredTodos(todos)
+         break 
+    }
+  }
+  //save to local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify([]))
+    
+  }
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]))
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'))
+      setTodos(todoLocal)
+    }
+  }
   return (
     <div className="App">
      <header>
        <h1>Coconut's TodoList</h1>
      </header>
-     <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-     <TodoList setTodos={setTodos} todos={todos}/>
+     <Form 
+     inputText={inputText} 
+     todos={todos} 
+     setTodos={setTodos} 
+     setInputText={setInputText}
+     setStatus={setStatus}
+     
+     />
+     <TodoList 
+     filteredTodos={filteredTodos} 
+     setTodos={setTodos} 
+     todos={todos}/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 
 
-const Form = ({setInputText, todos, setTodos, inputText}) => {
+const Form = ({setInputText, todos, setTodos, inputText, setStatus}) => {
   // ecrire ici js code et fct
   const inputTextHandler = (e) => {
-    console.log(e.target.value)
+    
     setInputText(e.target.value)
   }
   const submitTodoHandler = (e) => {
@@ -17,6 +17,9 @@ const Form = ({setInputText, todos, setTodos, inputText}) => {
     ])
     setInputText("")
   }
+  const statusHandler = (e) => {
+    setStatus(e.target.value)
+  }
   
   return (
      <form>
@@ -25,7 +28,7 @@ const Form = ({setInputText, todos, setTodos, inputText}) => {
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
